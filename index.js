68bad54b8435ced90d4848c5d0dead65dd13d276
anilvashisth <80627006+anilvashisth@users.noreply.github.com>
2025-05-17 05:06:07 +0530

    Node JS

--- index.js
+++ index.js
@@ -1,45 +1,9 @@
-//Type1
-/*const fs = require('fs');
+//console.log('testing!')
+const { format } = require ('date-fns');
+const {v4:uuid } = require('uuid');
 
-fs.readFile('./files/starter.txt',(err,data)=>{
-    if(err) throw err;
-    console.log(data.toString());
-})*/
+console.log(format(new Date(),'yyyymmdd\tHH:mm:ss'))
 
-//Type2
-const fs = require('fs');
-const path = require('path');
-//read a file
-fs.readFile(path.join(__dirname,'files','starter.txt'),'utf-8',(err,data)=>{
-    if(err) throw err;
-    console.log(data);
-
-})
-
-console.log('Hello....');
-//write a file
-
-fs.writeFile(path.join(__dirname,'files','reply.txt'),'Nice to meet you.',(err)=>{
-    if(err) throw err;
-    console.log('operation of file write complete');
-//append a file 
-    fs.appendFile(path.join(__dirname,'files','reply.txt'),'\n \n yes it is ,Nice to meet you.',(err)=>{
-    if(err) throw err;
-    console.log('operation of file Append complete');
-    //rename a file
-        fs.rename(path.join(__dirname,'files','reply.txt'), path.join(__dirname,'files','newReply.txt') ,(err)=>{
-        if(err) throw err;
-        console.log('operation of file rename complete');
-
-
-})
-
-})
-
-})
-//exit on uncaught Error
-process.on ('uncaughtException',err =>{
-console.log (`there was an uncaught error: ${err}`);
-process.exit(1);
-})
-
+//console.log('Jai Hind')
+console.log(uuid())
+console.log()
